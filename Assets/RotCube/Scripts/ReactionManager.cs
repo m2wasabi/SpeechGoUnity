8e9b7473d573784e0e9b69aea82baad57241eca2
m2wasabi <m2wasabi@gmail.com>
2017-07-03 03:16:27 +0900

    Shoot Speech Text

--- Assets/RotCube/Scripts/ReactionManager.cs
+++ Assets/RotCube/Scripts/ReactionManager.cs
@@ -9,6 +9,9 @@ public class ReactionManager : Singleton<ReactionManager>
 
     public AudioClip SpeechFeedbackSound;
     private AudioSource reactionAudioSource;
+    public GameObject TextObject;
+    private GameObject Camera;
+    public float BulletSpeed = 1000;
 
     // Use this for initialization
     void Start () {
@@ -26,20 +29,45 @@ public class ReactionManager : Singleton<ReactionManager>
             reactionAudioSource.spatialBlend = 1;
             reactionAudioSource.dopplerLevel = 0;
         }
+
+        Camera = GameObject.Find("HoloLensCamera");
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateTextObject("Hello!");
+        }
+    }
 
     public void Action (RecognitionResponse obj)
     {
         string text = obj.results[0].alternatives[0].transcript;
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        //{
+        //    Instantiate(TextObject, hit.point, Quaternion.identity);
+        //}
+
+        GenerateTextObject(text);
 
         if (text.IndexOf("最高") != -1)
         {
             reactionAudioSource.Play();
         }
+    }
+
+    private void GenerateTextObject(string word)
+    {
+        GameObject wordBullet = GameObject.Instantiate(TextObject);
+        Vector3 force;
+        force = Camera.transform.forward * BulletSpeed;
+        wordBullet.GetComponent<Rigidbody>().AddForce(force);
+        wordBullet.transform.position = Camera.transform.position;
+        TextMesh _text = wordBullet.GetComponent<TextMesh>();
+        _text.text = word;
     }
 }
