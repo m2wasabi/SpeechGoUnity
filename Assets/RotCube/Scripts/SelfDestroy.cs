b959310410c0f709fde41c153c1a8061ac8797e4
m2wasabi <m2wasabi@gmail.com>
2017-07-05 05:03:15 +0900

    Eliminate Explosion Particles

---
+++ Assets/RotCube/Scripts/SelfDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour {
+
+    public float EliminateTimer = 1.5f;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("Eliminate");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    IEnumerator Eliminate()
+    {
+        yield return new WaitForSeconds(EliminateTimer);
+        Destroy(gameObject);
+    }
+}
