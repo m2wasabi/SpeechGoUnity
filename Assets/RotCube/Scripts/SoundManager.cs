ef5e9c839faea5b6981576e1410c69d546a38d7a
m2wasabi <m2wasabi@gmail.com>
2017-07-14 11:30:35 +0900

    Add Sound Manager

---
+++ Assets/RotCube/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public enum State { Stop, Busy, Loop }
+    public State Status { get; private set; }
+
+    private AudioSource _audioSource;
+    public AudioClip[] Sounds;
+
+
+    // Use this for initialization
+    void Start () {
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            _audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        _audioSource.playOnAwake = false;
+        _audioSource.spatialBlend = 1;
+        _audioSource.dopplerLevel = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void Play(int index)
+    {
+        if (Sounds.Length > index && Sounds[index] != null && !_audioSource.isPlaying)
+        {
+            _audioSource.clip = Sounds[index];
+            _audioSource.Play();
+        }
+    }
+}
